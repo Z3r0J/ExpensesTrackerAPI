5b514d2211c98fd740a285bb2f0e4641cc7fcef8
Jean Carlos Reyes Encarnacion <tibboxcentral@gmail.com>
2023-09-17 14:20:48 -0400

    feat: Category Created Event feat: Transaction Added Event

---
+++ ExpensesTrackerAPI.Core.Domain/Entities/Categories/Events/CategoryCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using ExpensesTrackerAPI.Core.Domain.Primitives;
+
+namespace ExpensesTrackerAPI.Core.Domain.Entities.Categories.Events;
+
+public sealed class CategoryCreatedEvent : DomainEvent
+{
+    public Guid Id { get; private set; }
+
+    public CategoryCreatedEvent(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ ExpensesTrackerAPI.Core.Domain/Entities/Transactions/Events/TransactionAddedEvent.cs
@@ -0,0 +1,13 @@
+﻿using ExpensesTrackerAPI.Core.Domain.Primitives;
+
+namespace ExpensesTrackerAPI.Core.Domain.Entities.Transactions.Events;
+
+internal class TransactionAddedEvent : DomainEvent
+{
+    public Guid TransactionId { get; private set; } = Guid.Empty;
+
+    public TransactionAddedEvent(Guid Id)
+    {
+        TransactionId = Id;
+    }
+}
